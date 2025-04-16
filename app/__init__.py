00aa89023f2505ead88355cb73c447bc811f9634
kiprotich267 <enockierui@gmail.com>
2025-04-16 17:08:48 +0300

    modified the flask task app

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='c2ebfa0eb82b22723909d6a5940fca15ff7533928d69947b92192843be4e1e63'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///enock.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
+db = SQLAlchemy(app)
 
 
 
---
+++ app/models.py
@@ -0,0 +1,29 @@
+from app import db
+
+class User(db.Model): 
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100), nullable=False)  
+    tasks = db.relationship('Task', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+
+    
+
+class Task(db.Model): 
+    __tablename__ = 'tasks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Task {self.title}>"
