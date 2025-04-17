878e7edb904c151a03895d6b1d31967d95694443
kiprotich267 <enockierui@gmail.com>
2025-04-17 16:27:10 +0300

    added index html to templates

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import db
 
 class User(db.Model): 
-    __tablename__ = 'users'
+    
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
@@ -11,19 +11,19 @@ class User(db.Model):
     tasks = db.relationship('Task', backref='user', lazy=True)
 
     def __repr__(self):
-        return f"<User {self.username}>"
+        return f" {self.first_name} {self.last_name} "
 
 
     
 
 class Task(db.Model): 
-    __tablename__ = 'tasks'
+   
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
     completed = db.Column(db.Boolean, default=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"<Task {self.title}>"
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,15 @@
+
+from flask import render_template
 from app import app
+from app.models import User
+
+users = ["rop", "kirui", "kipngeno", "dorothy", "enock"]
+
+
 
 @app.route('/')
 def home():
-    return "<h1>Welcome to Task manager!</h1>"
+    users = User.query.all()
+    return render_template('index.html', users=users)
+
+
