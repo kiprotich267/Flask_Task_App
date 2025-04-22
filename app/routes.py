86f51c499139489bf56f6d758fe6d54304a610a1
kiprotich267 <enockierui@gmail.com>
2025-04-22 14:42:59 +0300

    added bootstrap and links

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,17 @@ users = ["rop", "kirui", "kipngeno", "dorothy", "enock"]
 
 @app.route('/')
 def home():
+    
+    return render_template('index.html', users=users, title='Home')
+
+@app.route('/tasks')
+def tasks():
+    
+    return render_template('tasks.html', users=users, title='Tasks')
+
+@app.route('/users')
+def users():
     users = User.query.all()
-    return render_template('index.html', users=users)
+    return render_template('users.html', users=users, title='Users')
 
 
