c9c908a9b64a61f65612409de747df435019ce11
kiprotich267 <enockierui@gmail.com>
2025-04-23 22:33:24 +0300

    added register and task input page

---
+++ app/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, BooleanField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
+class TaskForm(FlaskForm):
+   title = StringField('Task Title', validators=[DataRequired()])
+   description = TextAreaField('Task Description', validators=[DataRequired()])
+   completed = BooleanField('Completed', validators=[DataRequired()])
+   submit = SubmitField('Submit')
+
+class UserForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 
-from flask import render_template
-from app import app
-from app.models import User
+from flask import render_template, redirect, url_for
+from app import app,db
+from app.models import User, Task
+from app.forms import TaskForm
+from app.forms import UserForm
 
 users = ["rop", "kirui", "kipngeno", "dorothy", "enock"]
 
@@ -9,17 +11,43 @@ users = ["rop", "kirui", "kipngeno", "dorothy", "enock"]
 
 @app.route('/')
 def home():
-    
+    users = User.query.all()
+
+
     return render_template('index.html', users=users, title='Home')
 
-@app.route('/tasks')
+@app.route('/tasks', methods=['GET', 'POST'])
 def tasks():
+    form = TaskForm()
+    if form.validate_on_submit(): 
+        new_task = Task(
+            title=form.title.data,
+            description=form.description.data,
+            completed=form.completed.data
+        )
+        db.session.add(new_task)
+        db.session.commit()
+        tasks = Task.query.all()
+        return redirect(url_for('tasks'))
+   
     
-    return render_template('tasks.html', users=users, title='Tasks')
+    return render_template('tasks.html', users=users, title='Tasks', form=form, tasks=tasks )
 
-@app.route('/users')
-def users():
-    users = User.query.all()
-    return render_template('users.html', users=users, title='Users')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+   
+    form = UserForm()
+    if form.validate_on_submit():
+        new_user = User(
+            username=form.username.data,
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            password=form.password.data
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('home'))
+        
+    return render_template('register.html', users=users, title='Register', user_form=form)
 
 
